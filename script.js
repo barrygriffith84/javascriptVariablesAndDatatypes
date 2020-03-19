9bb256069b828268b1db9515060d330f6cb111d4
Barry Griffith <barrygriffith@outlook.com>
2020-03-19 19:22:08 -0400

    The first practice exercise completed

---
+++ script.js
@@ -0,0 +1,11 @@
+var temperature = 70;
+var conditions = "cloudy";
+var sunriseTime = "7:34 AM";
+var sunsetTime = "7:43 PM";
+
+console.log(temperature, conditions, sunriseTime, sunsetTime);
+
+var sentence = `The sun rises at ${sunriseTime} the temperature is ${temperature} it is {conditions} and the sun sets at ${sunsetTime}`;
+
+console.log(sentence);
+
