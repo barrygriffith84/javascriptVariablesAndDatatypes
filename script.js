a74bcf32663537db9e496be08950861cddc7fdcb
Barry Griffith <barrygriffith@outlook.com>
2020-03-27 21:22:44 -0400

    All exercises compelte

--- script.js
+++ script.js
@@ -293,3 +293,174 @@ console.log(kurtObject.hobbies[2]);
 console.log(`${kurtObject.firstName} ${kurtObject.lastName} has ${kurtObject.hobbies.length} main hobbies.  He likes to ${kurtObject.hobbies[0]}, ${kurtObject.hobbies[1]}, and ${kurtObject.hobbies[2]}`)
 
 
+
+// Use JavaScript to answer the following questions and log your answers to the console.
+
+//     What time was sunrise this morning in Nome, AK?
+//     What was the wind chill in Nome, AK when this data was generated?
+//     What was the visibility?
+//     What will be the high on Saturday the 8th?
+//     Use string template literals to build a sentence about the weather forecast for Saturday, Dec 15. (Example: "Saturday, December 15 will be Partly Cloudy with a high of 7 and a low of -3".)
+//         In the data, "Partly Cloudy" is capitalized, but in our sentence that doesn't really make sense. Look up the method to convert a string to lowercase.
+//     What is the average forecasted high temperature for the next seven days?
+
+var weatherData = {
+  "query": {
+   "count": 1,
+   "created": "2018-12-06T20:52:22Z",
+   "lang": "en-US",
+   "results": {
+    "channel": {
+     "units": {
+      "distance": "mi",
+      "pressure": "in",
+      "speed": "mph",
+      "temperature": "F"
+     },
+     "title": "Yahoo! Weather - Nome, AK, US",
+     "link": "http://us.rd.yahoo.com/dailynews/rss/weather/Country__Country/*https://weather.yahoo.com/country/state/city-2460286/",
+     "description": "Yahoo! Weather for Nome, AK, US",
+     "language": "en-us",
+     "lastBuildDate": "Thu, 06 Dec 2018 11:52 AM AKST",
+     "ttl": "60",
+     "location": {
+      "city": "Nome",
+      "country": "United States",
+      "region": " AK"
+     },
+     "wind": {
+      "chill": "14",
+      "direction": "68",
+      "speed": "36"
+     },
+     "atmosphere": {
+      "humidity": "90",
+      "pressure": "989.0",
+      "rising": "0",
+      "visibility": "11.0"
+     },
+     "astronomy": {
+      "sunrise": "11:38 am",
+      "sunset": "4:7 pm"
+     },
+     "image": {
+      "title": "Yahoo! Weather",
+      "width": "142",
+      "height": "18",
+      "link": "http://weather.yahoo.com",
+      "url": "http://l.yimg.com/a/i/brand/purplelogo//uh/us/news-wea.gif"
+     },
+     "item": {
+      "title": "Conditions for Nome, AK, US at 11:00 AM AKST",
+      "lat": "64.499474",
+      "long": "-165.405792",
+      "link": "http://us.rd.yahoo.com/dailynews/rss/weather/Country__Country/*https://weather.yahoo.com/country/state/city-2460286/",
+      "pubDate": "Thu, 06 Dec 2018 11:00 AM AKST",
+      "condition": {
+       "code": "15",
+       "date": "Thu, 06 Dec 2018 11:00 AM AKST",
+       "temp": "28",
+       "text": "Blowing Snow"
+      },
+      "forecast": [
+       {
+        "code": "16",
+        "date": "06 Dec 2018",
+        "day": "Thu",
+        "high": "29",
+        "low": "25",
+        "text": "Snow"
+       },
+       {
+        "code": "26",
+        "date": "07 Dec 2018",
+        "day": "Fri",
+        "high": "27",
+        "low": "22",
+        "text": "Cloudy"
+       },
+       {
+        "code": "28",
+        "date": "08 Dec 2018",
+        "day": "Sat",
+        "high": "22",
+        "low": "8",
+        "text": "Mostly Cloudy"
+       },
+       {
+        "code": "34",
+        "date": "09 Dec 2018",
+        "day": "Sun",
+        "high": "7",
+        "low": "-11",
+        "text": "Mostly Sunny"
+       },
+       {
+        "code": "28",
+        "date": "10 Dec 2018",
+        "day": "Mon",
+        "high": "3",
+        "low": "-11",
+        "text": "Mostly Cloudy"
+       },
+       {
+        "code": "28",
+        "date": "11 Dec 2018",
+        "day": "Tue",
+        "high": "10",
+        "low": "2",
+        "text": "Mostly Cloudy"
+       },
+       {
+        "code": "26",
+        "date": "12 Dec 2018",
+        "day": "Wed",
+        "high": "9",
+        "low": "5",
+        "text": "Cloudy"
+       },
+       {
+        "code": "28",
+        "date": "13 Dec 2018",
+        "day": "Thu",
+        "high": "14",
+        "low": "8",
+        "text": "Mostly Cloudy"
+       },
+       {
+        "code": "30",
+        "date": "14 Dec 2018",
+        "day": "Fri",
+        "high": "13",
+        "low": "-3",
+        "text": "Partly Cloudy"
+       },
+       {
+        "code": "30",
+        "date": "15 Dec 2018",
+        "day": "Sat",
+        "high": "7",
+        "low": "-3",
+        "text": "Partly Cloudy"
+       }
+      ],
+      "description": "<![CDATA[<img src=\"http://l.yimg.com/a/i/us/we/52/15.gif\"/>\n<BR />\n<b>Current Conditions:</b>\n<BR />Blowing Snow\n<BR />\n<BR />\n<b>Forecast:</b>\n<BR /> Thu - Snow. High: 29Low: 25\n<BR /> Fri - Cloudy. High: 27Low: 22\n<BR /> Sat - Mostly Cloudy. High: 22Low: 8\n<BR /> Sun - Mostly Sunny. High: 7Low: -11\n<BR /> Mon - Mostly Cloudy. High: 3Low: -11\n<BR />\n<BR />\n<a href=\"http://us.rd.yahoo.com/dailynews/rss/weather/Country__Country/*https://weather.yahoo.com/country/state/city-2460286/\">Full Forecast at Yahoo! Weather</a>\n<BR />\n<BR />\n<BR />\n]]>",
+      "guid": {
+       "isPermaLink": "false"
+      }
+     }
+    }
+   }
+  }
+ }
+
+console.log(weatherData["query"]["results"]["channel"]["astronomy"]["sunrise"]);
+console.log(weatherData["query"]["results"]["channel"]["wind"]["chill"]);
+console.log(weatherData["query"]["results"]["channel"]["atmosphere"]["visibility"]);
+console.log(weatherData["query"]["results"]["channel"]["item"]["forecast"][2]["high"]);
+
+console.log(`Today it will be ${weatherData["query"]["results"]["channel"]["item"]["forecast"][9]["text"].toLowerCase()}, we'll have a high of ${weatherData["query"]["results"]["channel"]["item"]["forecast"][9]["high"]} and a low of ${weatherData["query"]["results"]["channel"]["item"]["forecast"][9]["low"]}`);
+
+for(var i = 0; i < 7; i++){
+  console.log(`Day ${i+1} high: ${weatherData["query"]["results"]["channel"]["item"]["forecast"][i]["high"]}`)
+} 
